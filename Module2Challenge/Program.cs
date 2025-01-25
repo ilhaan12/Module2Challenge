bc71e855a29530916bad0d7d9557b96ecd6da85d
ilhaan12 <168375285+ilhaan12@users.noreply.github.com>
2025-01-25 20:38:31 +0000

    initial commit

---
+++ Module2Challenge/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!"); 
+
+        Console.Write("Enter the number of miles driven: ");
+        double milesDriven = Convert.ToDouble(Console.ReadLine());
+
+    
+        Console.Write("Enter the amount of gasoline used (in gallons): ");
+        double gallonsUsed = Convert.ToDouble(Console.ReadLine());
+
+      
+        double mpg = milesDriven / gallonsUsed;
+
+      
+        Console.WriteLine($"The miles per gallon (MPG) is: {mpg:F2}");
+    
